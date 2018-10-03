e2187836da8d5f93fce6dc299dc8d1bf175f94c0
tania <tani.zhelezniak@gmail.com>
2018-10-03 13:07:38 +0300

    material-tabs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
 
-  all = 'all';
-  constructor() { }
+    tabs = {
+        tab_1: 'all',
+        tab_2: 'Design',
+        tab_3: 'Branding',
+        tab_4: 'Graphic',
+        tab_5: 'Animation',
+        tab_6: 'Illustration',
+        tab_7: 'Photography'
+    }
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
