c4e949a315a9e6def85d665ecaa864b9d9925d3f
tania <tani.zhelezniak@gmail.com>
2018-10-04 15:04:47 +0300

    material-tabs

--- src/app/dashitem/dashitem.component.ts
+++ src/app/dashitem/dashitem.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {
-    Image,
-    LineLayout,
-    PlainGalleryConfig,
-    PlainGalleryStrategy,
-} from 'angular-modal-gallery';
+// import {
+//     Image,
+//     LineLayout,
+//     PlainGalleryConfig,
+//     PlainGalleryStrategy,
+// } from 'angular-modal-gallery';
 
 @Component({
     selector: 'app-dashitem',
@@ -14,33 +14,33 @@ import {
 export class DashitemComponent implements OnInit {
     constructor() {
     }
-    plainGalleryRow: PlainGalleryConfig = {
-        strategy: PlainGalleryStrategy.ROW,
-        layout: new LineLayout({width: '100%', height: '100%'}, {length: 1, wrap: true}, 'flex-start')
-    };
-
-    images: Image[] = [
-        new Image(
-            0,
-            { img: '../assets/img/jpg/1.jpg' }
-        ),
-        new Image(
-            1,
-            { img: '../assets/img/jpg/2.jpg' }
-        ),
-        new Image(
-            2,
-            { img: '../assets/img/jpg/3.jpg' }
-        ),
-        new Image(
-            3,
-            { img: '../assets/img/jpg/5.jpg' }
-        ),
-        new Image(
-            4,
-            { img: '../assets/img/jpg/6.jpg' }
-        )
-    ];
+    // plainGalleryRow: PlainGalleryConfig = {
+    //     strategy: PlainGalleryStrategy.ROW,
+    //     layout: new LineLayout({width: '100%', height: '100%'}, {length: 1, wrap: true}, 'flex-start')
+    // };
+    //
+    // images: Image[] = [
+    //     new Image(
+    //         0,
+    //         { img: '../assets/img/jpg/1.jpg' }
+    //     ),
+    //     new Image(
+    //         1,
+    //         { img: '../assets/img/jpg/2.jpg' }
+    //     ),
+    //     new Image(
+    //         2,
+    //         { img: '../assets/img/jpg/3.jpg' }
+    //     ),
+    //     new Image(
+    //         3,
+    //         { img: '../assets/img/jpg/5.jpg' }
+    //     ),
+    //     new Image(
+    //         4,
+    //         { img: '../assets/img/jpg/6.jpg' }
+    //     )
+    // ];
 
     ngOnInit() {
     }
