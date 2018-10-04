874023f318d63a2cc98d37b5495c7f302b9e5931
tania <tani.zhelezniak@gmail.com>
2018-10-04 16:17:18 +0300

    material-tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MainNavComponent } from './main-nav/main-nav.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatInputModule, MatTabsModule} from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { DashitemComponent } from './dashitem/dashitem.component';
+
 
 import 'hammerjs';
 import 'mousetrap';
@@ -22,8 +22,7 @@ import {ModalGalleryModule} from 'angular-modal-gallery';
     AppComponent,
     HeaderComponent,
     MainNavComponent,
-    DashboardComponent,
-    DashitemComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {
-    Image,
-    LineLayout,
-    PlainGalleryConfig,
-    PlainGalleryStrategy,
-} from 'angular-modal-gallery';
+
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -22,33 +17,10 @@ export class DashboardComponent implements OnInit {
         tab_7: 'Photography'
     }
 
-    plainGalleryRow: PlainGalleryConfig = {
-        strategy: PlainGalleryStrategy.ROW,
-        layout: new LineLayout({width: '100%', height: '100%'}, {length: 1, wrap: true}, 'flex-start')
-    };
 
-    images: Image[] = [
-        new Image(
-            0,
-            { img: '../assets/img/jpg/1.jpg' }
-        ),
-        new Image(
-            1,
-            { img: '../assets/img/jpg/2.jpg' }
-        ),
-        new Image(
-            2,
-            { img: '../assets/img/jpg/3.jpg' }
-        ),
-        new Image(
-            3,
-            { img: '../assets/img/jpg/5.jpg' }
-        ),
-        new Image(
-            4,
-            { img: '../assets/img/jpg/6.jpg' }
-        )
-    ];
+
+
+
     constructor() {
     }
 
