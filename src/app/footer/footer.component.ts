5218f6a395d2ffa9af5dcd57ec9801dd8cc417e9
tania <tani.zhelezniak@gmail.com>
2018-10-16 11:30:24 +0300

    material-tabs

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+    gridImg = [
+        {
+            src: '/assets/img/png/1.png'
+        },
+        {
+            src: '/assets/img/png/2.png'
+        },
+        {
+            src: '/assets/img/png/3.png'
+        },
+        {
+            src: '/assets/img/png/4.png'
+        },
+        {
+            src: '/assets/img/png/5.png'
+        },
+        {
+            src: '/assets/img/png/6.png'
+        },
+        {
+            src: '/assets/img/png/7.png'
+        },
+        {
+            src: '/assets/img/png/8.png'
+        },
+        {
+            src: '/assets/img/png/9.png'
+        },
+        {
+            src: '/assets/img/png/10.png'
+        },
+        {
+            src: '/assets/img/png/11.png'
+        },
+        {
+            src: '/assets/img/png/12.png'
+        },
+    ]
 
-  ngOnInit() {
-  }
+
+    constructor() {
+    }
+
+
+    ngOnInit() {
+    }
 
 }
