355b11153996b1cb60cb5b3142c50deb8c8f5368
tania <tani.zhelezniak@gmail.com>
2018-10-22 10:50:10 +0300

    search

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
     title = 'Welcome Ladies & Gents to Calm Website.';
     slogan = 'theme more stylish than any other on ThemeForest.';
+    active = false;
+    expand = false;
+
+    onClick(){
+        this.active = !this.active;
+        this.expand = !this.expand;
+    }
+
+
     constructor() { }
 
     ngOnInit() {
     }
+
+
+
 
 }
