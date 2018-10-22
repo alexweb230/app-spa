cf2cd796c3494c990f4f21b81c523e9534b7e3ff
tania <tani.zhelezniak@gmail.com>
2018-10-22 11:34:51 +0300

    search

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,10 +10,14 @@ export class HeaderComponent implements OnInit {
     slogan = 'theme more stylish than any other on ThemeForest.';
     active = false;
     expand = false;
+    type = 'button';
 
     onClick(){
         this.active = !this.active;
         this.expand = !this.expand;
+        setTimeout(function () {
+
+        }, 1000);
     }
 
 
