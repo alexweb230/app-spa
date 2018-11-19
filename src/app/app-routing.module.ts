16fd7eb5a93c7832e63c892337dfc73b25818460
tania <tani.zhelezniak@gmail.com>
2018-11-19 14:13:15 +0200

    last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BlogComponent} from './blog/blog.component';
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
-
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'blog', component: BlogComponent},
     {path: 'home', component: DashboardComponent},
 ];
