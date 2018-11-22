6117abc86648f297b3ac139f6cd9f79d46c573e5
tania <tani.zhelezniak@gmail.com>
2018-11-22 12:57:13 +0200

    last

--- src/app/dashboard-detail/dashboard-detail.component.ts
+++ src/app/dashboard-detail/dashboard-detail.component.ts
@@ -1,38 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard-detail',
-  templateUrl: './dashboard-detail.component.html',
-  styleUrls: ['./dashboard-detail.component.scss']
+    selector: 'app-dashboard-detail',
+    templateUrl: './dashboard-detail.component.html',
+    styleUrls: ['./dashboard-detail.component.scss']
 })
 export class DashboardDetailComponent implements OnInit {
-  public dashId;
-  constructor(
-      private route: ActivatedRoute,
-      private router: Router,
-  ) { }
+    public dashId;
 
-  ngOnInit() {
+    constructor(private route: ActivatedRoute,
+                private router: Router,) {
+    }
 
-    // let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    //
-    // this.dashId = id;
-
-    this.route.paramMap.subscribe((params: ParamMap) => {
-      let id = parseInt (params.get('id'));
-
-        this.dashId = id;
-    });
-  }
+    ngOnInit() {
+        this.route.paramMap.subscribe((params: ParamMap) => {
+            let id = parseInt(params.get('id'));
+            this.dashId = id;
+        });
+    }
 
 
-  goPrevious(){
-    let previousId = this.dashId - 1;
-    this.router.navigate(['/home', previousId]);
-  }
+    goPrevious() {
+        let previousId = this.dashId - 1;
+        this.router.navigate(['/home', previousId]);
+    }
 
-    goNext(){
+    goNext() {
         let nextId = this.dashId + 1;
         this.router.navigate(['/home', nextId]);
     }
