aa82d02b3d67b1f85777bcbbb0dc5b8ce15c9752
tania <tani.zhelezniak@gmail.com>
2018-11-22 12:05:09 +0200

    last

--- src/app/dashboard-detail/dashboard-detail.component.ts
+++ src/app/dashboard-detail/dashboard-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-detail',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-detail.component.scss']
 })
 export class DashboardDetailComponent implements OnInit {
-
-  constructor() { }
+  public dashId;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    let id = parseInt(this.route.snapshot.paramMap.get('id'));
+
+    this.dashId = id;
   }
 
 }
